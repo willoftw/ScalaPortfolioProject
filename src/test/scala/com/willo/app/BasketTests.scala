8b8507aaee037648de04cb96a5ba56adefc8fb2c
Will Olner <william.olner@gmail.com>
2017-09-15 23:22:17 +0100

    added product and basket class, added product tests

--- src/test/scala/com/willo/app/BasketTests.scala
+++ src/test/scala/com/willo/app/BasketTests.scala
@@ -1,4 +1,4 @@
-package com.willo.test
+package com.willo.app
 
 import org.scalatest.FunSuite
 
@@ -12,18 +12,3 @@ class ShopTests extends FunSuite {
 
 }
 
-class Basket(products:List[String])
-{
-
-    val product_list = new HashMap[String, Product]
-
-    product_list.put("Cola", Product("Cola","Cold",0.50f,"Drink"))
-    product_list.put("Coffee", Product("Coffee","Hot",1.0f,"Drink"))
-    product_list.put("Cheese Sandwich", Product("Cheese Sandwich","Cold",2.0f,"Food"))
-    product_list.put("Steak Sandwich", Product("Steak Sandwich","Hot",4.0f,"Food"))
-
-    def getTotal()=
-    {
-        7.5
-    }
-}
