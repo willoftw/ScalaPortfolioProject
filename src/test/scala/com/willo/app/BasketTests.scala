b453d8692715fbc09929bc0b12ccecf231acf361
Will Olner <william.olner@gmail.com>
2017-09-15 23:16:21 +0100

    implemented test then container class

--- src/test/scala/com/willo/app/BasketTests.scala
+++ src/test/scala/com/willo/app/BasketTests.scala
@@ -14,8 +14,16 @@ class ShopTests extends FunSuite {
 
 class Basket(products:List[String])
 {
-        def getTotal()=
-        {
-            7.5
-        }
+
+    val product_list = new HashMap[String, Product]
+
+    product_list.put("Cola", Product("Cola","Cold",0.50f,"Drink"))
+    product_list.put("Coffee", Product("Coffee","Hot",1.0f,"Drink"))
+    product_list.put("Cheese Sandwich", Product("Cheese Sandwich","Cold",2.0f,"Food"))
+    product_list.put("Steak Sandwich", Product("Steak Sandwich","Hot",4.0f,"Food"))
+
+    def getTotal()=
+    {
+        7.5
+    }
 }
