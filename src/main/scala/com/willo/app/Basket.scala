189efbe54ee3265079ec9ff89b9f07814d715a21
Will Olner <william.olner@gmail.com>
2017-09-15 23:25:19 +0100

    added product and basket class, added product tests

---
+++ src/main/scala/com/willo/app/Basket.scala
@@ -0,0 +1,23 @@
+package com.willo.app
+
+import scala.collection.mutable.HashMap
+
+class Basket(products:List[String])
+{
+    val product_list = new HashMap[String, Product]
+
+    product_list.put("Cola", Product("Cola","Cold",0.50f,"Drink"))
+    product_list.put("Coffee", Product("Coffee","Hot",1.0f,"Drink"))
+    product_list.put("Cheese Sandwich", Product("Cheese Sandwich","Cold",2.0f,"Food"))
+    product_list.put("Steak Sandwich", Product("Steak Sandwich","Hot",4.0f,"Food"))
+
+    def getTotal()=
+    {
+        var sum:Float = 0
+        for (product <- products)
+        {
+            sum += (menu(product).price)
+        }
+        sum
+    }
+}
---
+++ src/main/scala/com/willo/app/Product.scala
@@ -0,0 +1,3 @@
+package com.willo.app
+
+case class Product(name: String, temperature: String,price: Float,foodtype: String)
